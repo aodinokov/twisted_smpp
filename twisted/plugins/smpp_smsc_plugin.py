c2027af088d6f23fd235e14b11d67b2285cccaab
Alex <mralex@fedora14.(none)>
2012-09-25 13:41:07 +0400

    implemented part of node protocol

--- twisted/plugins/smpp_smsc_plugin.py
+++ twisted/plugins/smpp_smsc_plugin.py
@@ -1,9 +1,11 @@
+import uuid
+
 from zope.interface import implements
 
 from twisted.python import usage, log
 from twisted.plugin import IPlugin
 from twisted.internet import reactor
-from twisted.internet.protocol import ServerFactory, Protocol
+from twisted.internet.error import ConnectionRefusedError
 from twisted.internet.defer import Deferred, inlineCallbacks, returnValue
 from twisted.internet.protocol import ClientCreator, ServerFactory, Protocol
 from twisted.application import internet, service
@@ -12,6 +14,144 @@ from txamqp.protocol import AMQClient
 from txamqp.client import TwistedDelegate
 from txamqp.content import Content
 import txamqp.spec
+
+class SmppServerNodeCtrlProtocol(AMQClient):
+    def __init__(self, host, port, login, password, vhost, spec):
+        self.host = host
+        self.port = port
+        self.login = login
+        self.password = password
+        self.vhost = vhost
+        self.spec = spec
+
+        AMQClient.__init__(self, 
+                           delegate = TwistedDelegate(),
+                           vhost = vhost,
+                           spec = txamqp.spec.load(spec))
+    
+    @inlineCallbacks
+    def start(self, *args, **kargs):
+        yield AMQClient.start(self, *args, **kargs)
+        self.channel_conn = yield self.channel(0)
+        self.channel_rx = yield self.channel(1)
+        yield self.channel_rx.channel_open()
+        self.queue_rx = yield self.channel_rx.queue_declare(exclusive=True, auto_delete=True)
+        
+        self.replies = {} # correlation_id:deferred
+        yield self.channel_rx.basic_consume(queue=self.queue_rx.queue, no_ack=True, consumer_tag='qtag')
+        self.queue = yield AMQClient.queue(self, 'qtag')
+        self.finish_defer = Deferred()
+        self._update_work_defer()
+    
+    def _update_work_defer(self):
+        log.msg("_update_work_defer")
+        self.cur_defer = self.queue.get()
+        self.cur_defer.addCallbacks(self.onPacket, lambda _: None)
+    
+    def work(self):
+        return self.finish_defer
+    
+    @inlineCallbacks
+    def cancel(self):
+        if not(self.cur_defer is None):
+            yield self.channel_conn.connection_close()
+            self.cur_defer.cancel()
+            self.cur_defer = None
+            self.finish_defer.callback(None)
+    
+    def onPacket(self, response):
+        self._update_work_defer()
+
+        # types of messages
+        if 'reply to' in response.content.properties: # call request
+            reply = Content(self.onCall(response))
+            reply["delivery mode"] = 1 # non-persistent
+            reply['correlation id'] = response.content.properties['correlation id']
+            self.channel_rx.basic_publish(exchange = '', 
+                                                routing_key = reply['reply to'],
+                                                content = reply)
+            return
+            
+        if 'correlation id' in response.content.properties: # call response
+            if response.content.properties['correlation id'] in self.replies:
+                self.replies[response.content.properties['correlation id']].callback(response.content.body)
+            return
+        
+        # just async message
+        self.onRecv(response)
+        return
+
+    # low level
+    def onCall(self, msg):
+        pass
+    
+    def onRecv(self, msg):
+        pass
+    
+    @inlineCallbacks
+    def call(self, exchange, routing_key, body):
+        correlation_id = str(uuid.uuid4())
+        
+        msg = Content(body)
+        msg["delivery mode"] = 1 # non-persistent
+        msg['correlation id'] = correlation_id
+        msg['reply to'] = self.queue_rx.queue
+        
+        d = Deferred()
+        self.replies[correlation_id] = d
+        
+        try:
+            yield self.channel_rx.basic_publish(exchange = exchange, 
+                                                routing_key = routing_key,
+                                                content = msg)
+        except Exception as e:
+            del self.replies[correlation_id]
+            raise e
+        
+        timer = reactor.callLater(10, d.errback, Exception("timeout"))
+        def clbck(response):
+            if timer.active(): timer.cancel()
+            if correlation_id in self.replies:
+                del self.replies[correlation_id]
+            return response
+        d.addBoth(clbck)
+        
+        res = yield d
+        returnValue(res)
+    
+    @inlineCallbacks
+    def send(self, exchange, routing_key, body):
+        msg = Content(body)
+        
+        msg["delivery mode"] = 1 # non-persistent
+        yield self.channel_rx.basic_publish(exchange = exchange, 
+                                            routing_key = routing_key,
+                                            content = msg)
+
+class SmppServerNode(SmppServerNodeCtrlProtocol):
+
+    def __init__(self, host, port, login, password, vhost, spec, exchange, routing_key):
+        self.exchange = exchange
+        self.routing_key = routing_key
+        SmppServerNodeCtrlProtocol.__init__(self, host, port, login, password, vhost, spec)
+
+
+    def onRecv(self, msg):
+        log.msg("received %s", msg.content.body)
+        
+    def onCall(self, msg):
+        log.msg("called %s", msg.content.body)
+        return msg.content.body
+
+    @inlineCallbacks
+    def register(self, name):
+        res = yield self.call(self.exchange, self.routing_key, "register " + str(name))
+        returnValue(res)
+        
+    @inlineCallbacks
+    def unregister(self):
+        res = yield self.call(self.exchange, self.routing_key, "unregister")
+        returnValue(res)
 
 
 class SmppServerNodeService(service.Service):
@@ -27,9 +167,8 @@ class SmppServerNodeService(service.Service):
         pass
 
 class SmppServerNodeCtrlService(service.Service):
-    def __init__(self, service, host, port, login, password, vhost,
-                 exchange, routing_key):
-        self.service = service
+    def __init__(self, node_service, host, port, login, password, vhost, exchange, routing_key, spec):
+        self.node_service = node_service
         self.host = host
         self.port = port
         self.login = login
@@ -37,45 +176,52 @@ class SmppServerNodeCtrlService(service.Service):
         self.vhost = vhost
         self.exchange = exchange
         self.routing_key = routing_key
+        self.spec = spec
          
     def startService(self):
-        self.defer = self.serviceWork()
+        self.defer = self.service()
     
     @inlineCallbacks
-    def send(self, channel, body):
-        msg = Content(body)
-        msg["delivery mode"] = 1 # non-persistent
-        yield channel.basic_publish(exchange = self.exchange, 
-                                    routing_key = self.routing_key,
-                                    content = msg)
-    
-    @inlineCallbacks
-    def serviceWork(self):
-        delegate = TwistedDelegate()
+    def service(self):
+        
         creator = ClientCreator(reactor, 
-                                AMQClient, 
-                                delegate = delegate, 
-                                vhost=self.vhost)
-        self.protocol = yield creator.connectTCP(self.host, self.port)
-        yield self.protocol.start({"LOGIN": self.login, "PASSWORD": self.password})
-        channelRx = yield self.protocol.channel(0)
-        channelTx = yield self.protocol.channel(1)
-        try:
-            yield channelRx.queue_declare(queue='', 
-                                          durable=False,
-                                          exclusive=True,
-                                          auto_delete=True)
-            # register on server
-            self.send(channelTx, "test")
-            # TODO: consume messages
-        except Exception as e:
-            pass
-        self.protocol = None
-        yield channelRx.connection_close()
+                                SmppServerNode,
+                                host = self.host, 
+                                port = self.port,
+                                login = self.login,
+                                password = self.password,
+                                vhost = self.vhost,
+                                spec = self.spec,
+                                exchange = self.exchange,
+                                routing_key = self.routing_key)
+        while True:
+            try:
+                self.protocol = yield creator.connectTCP(self.host, self.port)
+                yield self.protocol.start({"LOGIN": self.login, "PASSWORD": self.password})
+                log.msg("Connected")
+                try:
+                    yield self.protocol.register("host")
+                    yield self.protocol.work()
+                except Exception as e:
+                    log.msg("Exception %s" %(repr(e),))
+                log.msg("Disconnecting")
+                yield self.protocol.cancel()
+                log.msg("Disconnected")
+                break
+            except ConnectionRefusedError as e:
+                log.msg("Connection refused. Will try again after time-out...")
+                defer = Deferred()
+                timer = reactor.callLater(10, defer.callback, None)
+                defer.addBoth(lambda _: timer.cancel() if timer.active() else None)
+                yield defer
+            except Exception as e:
+                log.msg("Exception %s" %(repr(e),))
+                break
+
         returnValue(None)
     
     def stopService(self):
-        return self.defer.cancel()
+        return self.protocol.cancel()
 
 class SmppServerNodeOptions(usage.Options):
     optParameters = [
@@ -86,9 +232,8 @@ class SmppServerNodeOptions(usage.Options):
         ['password', None, "guest", 'The RabbitMQ password.'],
         ['exchange', None, "smppserv.direct", 'Exchange name to route.'],
         ['routing_key', None, "ctrl", 'Key (e.g. queue name) to route.'],
+        ['spec', None, "amqp0-8.stripped.rabbitmq.xml", 'Spec file.'],
         ]
-
-
 
 class SmppServerNodeServiceMaker(object):
 
@@ -104,14 +249,15 @@ class SmppServerNodeServiceMaker(object):
         node_service = SmppServerNodeService()
         node_service.setServiceParent(top_service)
 
-        node_ctrl_service = SmppServerNodeCtrlService(service = node_service, 
+        node_ctrl_service = SmppServerNodeCtrlService(node_service = node_service, 
                                                       host = options['host'], 
                                                       port = int(options['port']), 
                                                       login = options['login'], 
                                                       password = options['password'], 
                                                       vhost = options['vhost'],
                                                       exchange = options['exchange'], 
-                                                      routing_key = options['routing_key'])
+                                                      routing_key = options['routing_key'],
+                                                      spec = options['spec'])
         node_ctrl_service.setServiceParent(top_service)
 
         return top_service
